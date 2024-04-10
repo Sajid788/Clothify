1ff191732e959e059cc7c04141ec92c0ef3d1065
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-04-10 10:50:56 +0530

    HomePage done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Footer from './components/user/footer/Footer';
 import Routing from './routing/Routing';
 
 function App() {
   return (
     <div className="App">
      <Routing/>
+     <Footer/>
     </div>
   );
 }
