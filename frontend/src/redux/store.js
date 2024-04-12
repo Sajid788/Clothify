0aefe2af064cacdb397625cff4f6e27bc65e5501
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-04-12 21:08:49 +0530

    Store updated

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -8,12 +8,14 @@ import {
 
   import { productReducer } from './User_Redux/products/reducer';
   import { authReducer } from './User_Redux/authentication/reducer';
+  import { cartReducer } from './User_Redux/cart/reducer';
 
   const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   
   const rootReducer = combineReducers({
     authReducer,
     productReducer,
+    cartReducer,
   });
   export const store = legacy_createStore(
     rootReducer,
