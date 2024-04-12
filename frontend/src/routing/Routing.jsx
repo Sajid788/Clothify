cf924e4fc331bbd3e19c6d70c3950b6c810c58f5
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-04-12 20:52:08 +0530

    Login token issue fixed & login done

--- frontend/src/routing/Routing.jsx
+++ frontend/src/routing/Routing.jsx
@@ -1,12 +1,14 @@
 import React, { Route, Routes } from "react-router-dom";
 import Homepage from "../pages/HomePage";
 import Signup from "../pages/Signup";
+import Login from "../pages/Login";
 
 const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
