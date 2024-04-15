b81df0862a2e0467259b6b5f47ec74c2d923fc16
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-04-15 10:09:53 +0530

    server side token issue fixed

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const { userRouter } = require("./routes/user_routes");
 const { adminRouter } = require('./routes/admin_routes');
 const { productRouter } = require('./routes/product_routes');
 const { cartRouter } = require('./routes/cart_routes');
-const { authorizedMiddleware } = require('./middleware/authorizedMiddleware');
+// const { authorizedMiddleware } = require('./middleware/authorizedMiddleware');
 const { orderRouter } = require('./routes/order_routes');
 const { WishlistRouter } = require('./routes/wishlist_routes');
 
@@ -24,9 +24,9 @@ app.get("/", (req, res) => {
 app.use("/user", userRouter);
 app.use('/admin', adminRouter);
 app.use('/product', productRouter);
-app.use('/cart', authorizedMiddleware, cartRouter);
+app.use('/cart',  cartRouter);
 app.use('/order', orderRouter);
-app.use('/wishlist', authorizedMiddleware, WishlistRouter);
+app.use('/wishlist',  WishlistRouter);
 
 // Not found route
 app.use((req, res) => {
