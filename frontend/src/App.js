1f7e2c4b8d7285b22ce51bd06dc4836e608b4bf4
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-04-15 19:19:08 +0530

    Readme update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import Navbar from './components/user/navbar/Navbar';
 import Routing from './routing/Routing';
 import Footer from './components/user/footer/Footer';
 
-
 function App() {
   return (
     <div className="App">
