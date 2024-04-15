e61e16a13876ea821af1fc8f40b3a38013d96aff
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-04-15 13:21:40 +0530

    Able to order product

--- frontend/src/routing/Routing.jsx
+++ frontend/src/routing/Routing.jsx
@@ -9,6 +9,7 @@ import SingleProduct from "../pages/SingleProduct";
 import CartPage from "../pages/Cart";
 import PrivateRoute from "../components/hoc/PrivateRoute";
 import WishlistPage from "../pages/WishlistPage";
+import CheckoutPage from "../pages/CheckoutPage";
 
 const Routing = () => {
   return (
@@ -20,8 +21,9 @@ const Routing = () => {
       <Route path="/women" element={<WomenPage />} />
       <Route path="/kids" element={<KidPage />} />
       <Route path="/product/:id" element={<SingleProduct />} />
-      <Route path="/cart" element={ <PrivateRoute>{" "}<CartPage /></PrivateRoute>} />
+      <Route path="/cart" element={ <PrivateRoute><CartPage /></PrivateRoute>} />
       <Route path="/wishlist" element={<PrivateRoute><WishlistPage /> </PrivateRoute>} />
+      <Route path="/checkout" element={<PrivateRoute> <CheckoutPage /></PrivateRoute>} />
     </Routes>
   );
 };
